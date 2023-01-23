6cab707293ab66550e4fb5d3fd524c5fb255920d
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2023-01-23 11:07:18 -0300

    Enviando os testes de unidade

---
+++ test/conta_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lib_ci_actions/conta.dart';
+
+void main() {
+  test('Deve validar o saldo quando o deposito for chamado', () {
+    final Conta conta = Conta(
+      numeroConta: 58884,
+      titular: 'André da Silva',
+    );
+
+    conta.depositar(100.00);
+    expect(conta.saldo, 100.00);
+
+    conta.depositar(50.00);
+    expect(conta.saldo, 150.00);
+
+    conta.depositar(5000.00);
+    expect(conta.saldo, 5150.00);
+  });
+
+  test('Deve validar o saldo quando o saque for chamado', () {
+    final Conta conta = Conta(
+      numeroConta: 58884,
+      titular: 'André da Silva',
+    );
+
+    conta.sacar(100.00);
+    expect(conta.saldo, -100.00);
+
+    conta.sacar(100.00);
+    expect(conta.saldo, -200.00);
+
+    conta.sacar(400.00);
+    expect(conta.saldo, -600.00);
+  });
+
+  test('Deve validar o saldo quando as tranferencias forem chamadas', () {
+    final Conta conta = Conta(
+      numeroConta: 58884,
+      titular: 'André da Silva',
+    );
+
+    conta.tranferir(2000.00);
+    expect(conta.saldo, -2000.00);
+
+    conta.tranferir(100.00);
+    expect(conta.saldo, -2100.00);
+
+    conta.tranferir(400.00);
+    expect(conta.saldo, -2500.00);
+  });
+}
